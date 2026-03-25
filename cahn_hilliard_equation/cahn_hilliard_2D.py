9d410f2111290a645f49add9662306e8ccc79284
Fiorello Mino <fiorellomino@192.168.1.54>
2026-03-26 00:20:06 +0100

    Aggiungo Cahn-Hilliard 2D

--- cahn_hilliard_equation/cahn_hilliard_2D.py
+++ cahn_hilliard_equation/cahn_hilliard_2D.py
@@ -5,26 +5,34 @@ import matplotlib.pyplot as plt
 #                    PARAMETRI SIMULAZIONE
 # ============================================================
 
-beta = 10
-T = 3600
+M = 10
+T = 100000
 L = 1
 N = 100
 dx = L / (N - 1)
 dy = L / (N - 1)
 delta = dx
-epsilon = 5 * delta
-dt = 0.2 * delta**2 / (beta * epsilon)
-
-# Nota per seconda simulazione: T=7200, epsilon=3*delta, dt=0.1*(...)
+epsilon = 3 * delta
+dt = dx**4 / (35 * M)
 
 
 # -----------------------------------------------------
 #           CONDIZIONE INIZIALE
 # -----------------------------------------------------
 
+# Profilo random
 phi = np.random.rand(N, N)
 phi_new = np.empty_like(phi)
 phi_initial = np.copy(phi)
+
+# Profilo 2
+# x = np.linspace(0, 1, 100)
+# y = np.linspace(0, 1, 100)
+# X, Y = np.meshgrid(x, y)
+# R0 = 0.1
+# phi = np.sqrt(X**2 + Y**2)
+# phi_new = np.empty_like(phi)
+# phi_initial = np.copy(phi)
 
 
 # -----------------------------------------------------
@@ -32,7 +40,7 @@ phi_initial = np.copy(phi)
 # -----------------------------------------------------
 
 energy = np.zeros(T)  # Evoluzione energia nel tempo
-
+phi_integral = np.zeros(T)  # Integrale di phi su tutto il dominio
 
 # -----------------------------------------------------
 #           CONFIGURAZIONE PLOT
@@ -43,7 +51,7 @@ steps = [0, T // 3, 2 * T // 3, T - 1]  # Momenti chiave per snapshot
 
 
 # ============================================================
-#                 INTEGRAZIONE ALLEN-CAHN
+#                INTEGRAZIONE CAHN-HILLIARD
 # ============================================================
 
 for n in range(T):
@@ -54,8 +62,15 @@ for n in range(T):
     # Calcola energia libera
     energy[n] = (2.0 * 18.0 / epsilon) * np.sum(phi**2 * (1 - phi**2))
 
-    # Laplaciano discreto
-    laplacian = (
+    # Calcola integrale di phi
+    phi_integral[n] = np.sum(phi)
+
+    # -----------------------------------------------------
+    #            CALCOLO DI MU
+    # -----------------------------------------------------
+
+    # Laplaciano discreto di phi
+    laplacian_phi = (
         np.roll(phi, 1, axis=0)
         + np.roll(phi, -1, axis=0)
         + np.roll(phi, 1, axis=1)
@@ -63,11 +78,27 @@ for n in range(T):
         - 4 * phi
     ) / delta**2
 
-    # Derivata del otenziale double well
+    # Derivata del potenziale double well
     w_prime = (36.0 / epsilon) * phi * (1 + 2 * phi**2 - 3 * phi)
 
+    # Calcolo di mu
+    mu = -epsilon * laplacian_phi + w_prime
+
+    # -----------------------------------------------------
+    #           STEP ESPLICITO PER PHI
+    # -----------------------------------------------------
+
+    # Laplaciano discreto di mu
+    laplacian_mu = (
+        np.roll(mu, 1, axis=0)
+        + np.roll(mu, -1, axis=0)
+        + np.roll(mu, 1, axis=1)
+        + np.roll(mu, -1, axis=1)
+        - 4 * mu
+    ) / delta**2
+
     # Step esplicito
-    phi_new = phi + dt * beta * (epsilon * laplacian - w_prime)
+    phi_new = phi + dt * M * laplacian_mu
 
     # Swap tra phi e phi_new in modo che phi al passo n+1 sia phi_new
     phi, phi_new = phi_new, phi
@@ -80,9 +111,9 @@ for n in range(T):
 fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
 for ax, n in zip(axes.ravel(), steps):
-    im = ax.imshow(snapshots[n], origin="lower", extent=[0, L, 0, L])
+    im = ax.imshow(snapshots[n], origin="lower", extent=[0, L, 0, L], cmap="coolwarm")
     t_fin = n * dt
-    ax.set_title(rf"$\phi(x,y,t={t_fin:.3f})$")
+    ax.set_title(rf"$\phi(x,y,t={t_fin:.7f})$")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     fig.colorbar(im, ax=ax)
@@ -95,12 +126,27 @@ plt.show()
 #           EVOLUZIONE ENERGIA
 # -----------------------------------------------------
 
-n_steps = np.arange(len(energy))
+# n_steps = np.arange(len(energy))
+# t = n_steps * dt
+# plt.figure(figsize=(6, 4))
+# plt.plot(t, energy, "-o", markersize=2)
+# plt.xlabel("t")
+# plt.ylabel("E(t)")
+# plt.title("Energy")
+# plt.tight_layout()
+# plt.show()
+
+
+# -----------------------------------------------------
+#           CONSERVAZIONE DELL'INTEGRALE DI PHI
+# -----------------------------------------------------
+
+n_steps = np.arange(len(phi_integral))
 t = n_steps * dt
 plt.figure(figsize=(6, 4))
-plt.plot(t, energy, "-o", markersize=2)
+plt.plot(t, phi_integral, "-o", markersize=1)
 plt.xlabel("t")
-plt.ylabel("E(t)")
-plt.title("Energy")
+plt.ylabel(r"Integral of $\phi$")
+plt.title(r"Mass conservation")
 plt.tight_layout()
 plt.show()
