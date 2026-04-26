e066b90144af64370697ff55f71b4bcfa57dd3a7
Fiorello Mino <fiorellomino@192.168.1.54>
2026-04-26 21:15:56 +0200

    stabilità su mu

--- cahn_hilliard_equation/params.py
+++ cahn_hilliard_equation/params.py
@@ -1,14 +1,14 @@
 # params.py
 
-N = 64
+N = 128
 dx = 1 / N
 
 dt = 1e-6
-n_steps = 10_000_000
-steps_per_save = 100_000
+n_steps = 100_000_000
+steps_per_save = 1_000_000
 
-epsilon = 5 * dx
+epsilon = 10 * dx
 M0 = 5e-5
 
 out_dir = "snapshots"
-live_plot = True
+live_plot = False
