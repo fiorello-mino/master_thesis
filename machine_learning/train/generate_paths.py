422b7186873c7757b47349441f986f174dfda659
fiorello-mino <fiorellomino01@gmail.com>
2026-05-15 12:13:41 +0200

    s

--- machine_learning/train/generate_paths.py
+++ machine_learning/train/generate_paths.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 import numpy as np
-base_dir = Path("/data/fiorello/dataset")
-output_txt = "validation_set_from_start.txt"
+base_dir = Path("/data/fiorello/dataset_128")
+output_txt = "test_set_128.txt"
 
-n_folders = 1000          # 0000 ... 0999
+n_folders = 200          # 0000 ... 0999
 n_files_total = 201       # 0000.npy ... 0200.npy
 #n_selected = 50           # vogliamo 50 file per cartella
 
@@ -11,10 +11,10 @@ n_files_total = 201       # 0000.npy ... 0200.npy
 #indices = list(range(0, 200, 4))[:50]
 
 with open(output_txt, "w") as f:
-    for folder_idx in range(800, n_folders):
+    for folder_idx in range(160, n_folders):
         folder = base_dir / f"{folder_idx:04d}"
-        start_index = 1
-        indices = list(range(start_index, start_index + 100))
+        start_index = np.random.randint(1,151)
+        indices = list(range(start_index, start_index + 50))
         paths = [str(folder / f"{file_idx:04d}.npy") for file_idx in indices]
         f.write(" ".join(paths) + "\n")
 
--- machine_learning/train/launch_training.sh
+++ machine_learning/train/launch_training.sh
@@ -3,7 +3,7 @@
 python3 /home/fiorello/CRANE/train.py \
 	--device  'cuda:0' \
 	--padding 'circular' \
-	--size 64 \
+	--size 128 \
 	--seed 666 \
 	--epochs 500 \
 	--nocrop \
@@ -11,9 +11,9 @@ python3 /home/fiorello/CRANE/train.py \
 	--lr 5e-5 \
 	--batch 3 \
 	--weightd 0e-4 \
-	--train_set 'training_set_from_start.txt' \
-	--valid_set 'validation_set_from_start.txt' \
-	--id 'test_lr_5e-5_hl_3_from_start' \
+	--train_set 'train_set_128.txt' \
+	--valid_set 'test_set_128.txt' \
+	--id 'lr_5e-5_hl_3_128' \
 	--logfreq 1 \
 	--kernel_size 5 \
 	--hidden 3 \
@@ -21,12 +21,12 @@ python3 /home/fiorello/CRANE/train.py \
 	--nproc 4 \
 	--num_params 0 \
 	--subseq_min 1 \
-	--subseq_max 99 \
+	--subseq_max 49 \
 	--ramp \
-	--ramp_length 98 \
+	--ramp_length 48 \
 	--reflection \
 	--noise_reg 0.0125 \
 	--rotation90 \
 	--divergence \
-	--dual
-	#--reload_model '/home/fiorello/master_thesis/machine_learning/train/train_logs/test_lr_5e-5_hl_3/model/epoch_487.pt' 
+	--dual \
+	--reload_model '/home/fiorello/master_thesis/machine_learning/train/train_logs/test_lr_5e-5_hl_3/model/epoch_487.pt' 
