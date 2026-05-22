008a64d78babb6637a5f772dacfb62acb38fec50
Fiorello Mino <fiorellomino@192.168.1.54>
2026-05-22 17:02:51 +0200

    script test aggiornato

--- cahn_hilliard_equation/scripts/postprocess_energy.py
+++ cahn_hilliard_equation/scripts/postprocess_energy.py
@@ -7,37 +7,61 @@ from cahn_hilliard.free_energy import total_free_energy, w_field
 from cahn_hilliard.operators import grad_2D
 import cahn_hilliard.parameters as p
 
-snap_dir = Path("/home/fiorello/master_thesis/cahn_hilliard_equation/0021")
+snap_dir_true = Path("/data/fiorello/dataset_random_fraction_phase1/0021")
+snap_dir_pred = Path("/data/fiorello/external_test_random_mean_value/le_5e-5_hl_3_train_from_10/0021/pred_sequence_npy")
 
+# Carico npy true
 files = []
-for f in snap_dir.glob("*.npy"):
-    files.append(f)
+for f in snap_dir_true.glob("*.npy"):
+    if int(f.stem) >= 10:
+        files.append(f)
 
 files.sort()
 
-snapshots = []
+snapshots_true = []
 for f in files:
-    snapshots.append(np.load(f, allow_pickle=False))
+    snapshots_true.append(np.load(f, allow_pickle=False))
 
-snapshots = np.array(snapshots)
+snapshots_true = np.array(snapshots_true)
 
+# Carico npy pred
+files_pred = []
+for f in snap_dir_pred.glob("*.npy"):
+    files_pred.append(f)
+
+files_pred.sort()
+
+snapshots_pred = []
+for f in files:
+    snapshots_pred.append(np.load(f, allow_pickle=False))
+
+snapshots_pred = np.array(snapshots_pred)
+    
+    
 n_snap, ny, nx = snapshots.shape
 times = np.empty(n_snap, dtype=float)
-energies = np.empty(n_snap, dtype=float)
+energies_true = np.empty(n_snap, dtype=float)
+energies_pred = np.empty(n_snap, dtype=float)
 
 for i in range(n_snap):
     
-    phi = snapshots[i]
-    times[i] = i * p.steps_per_save * p.dt
-    energies[i] = total_free_energy(phi = phi, epsilon = p.epsilon, dx = p.dx)
+    phi_true = snapshots_true[i]
+    phi_pred = snapshots_pred[i]
+    times[i] = (i+10) * p.steps_per_save * p.dt
+    energies_true[i] = total_free_energy(phi = phi_true, epsilon = p.epsilon, dx = p.dx)
+    energies_pred[i] = total_free_energy(phi = phi_pred, epsilon = p.epsilon, dx = p.dx)
     
 
-plt.figure()
-plt.plot(times, energies, marker='o', linestyle='-')
+plt.figure(figsize=(7, 5))
+
+plt.plot(times, energies_true, label='True energy', color='blue', linewidth=1.5)
+plt.plot(times, energies_pred, label='Pred energy', color='red', linewidth=1.5, linestyle='--')
+
 plt.xlabel('Time')
 plt.ylabel('Free energy')
-plt.title('Free energy vs time')
+plt.title('True vs Pred free energy')
 plt.grid(True)
+plt.legend()
 plt.tight_layout()
 
-plt.savefig('energy_vs_time.png', dpi=300)
+plt.savefig('energy_true_vs_pred.png', dpi=300)
