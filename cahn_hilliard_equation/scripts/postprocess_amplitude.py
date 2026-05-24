95b02492da6d384f930fbdb2020b72ab9d3f530d
Fiorello Mino <fiorellomino@192.168.1.54>
2026-05-25 00:13:41 +0200

    amp cosine

--- cahn_hilliard_equation/scripts/postprocess_amplitude.py
+++ cahn_hilliard_equation/scripts/postprocess_amplitude.py
@@ -26,8 +26,8 @@ def load_snapshots_array(snap_dir="/data/fiorello/run_cosine", first=0, last=100
 times, snapshots = load_snapshots_array("/data/fiorello/run_cosine", first=0, last=100, dt=p.dt)
 n_snap, ny, nx = snapshots.shape
 
-x = (np.arange(nx) + 0.5) * p.dx
-y = (np.arange(ny) + 0.5) * p.dx
+x = (np.arange(nx) + 0.5) * 1.0/128
+y = (np.arange(ny) + 0.5) * 1.0/128
 X, Y = np.meshgrid(x, y)
 
 level = 0.5
@@ -51,11 +51,6 @@ for i in range(n_snap):
 
     y_min = all_points[:, 1].min()
     y_max = all_points[:, 1].max()
-    
-    if i == 0:
-        print("y_min =", y_min)
-        print("y_max =", y_max)
-        print("y_max - y_min =", y_max - y_min)
 
     amps[i] = 0.5 * (y_max - y_min)
     energies[i] = total_free_energy(phi=phi, epsilon=5.0/64, dx=1.0/128)
