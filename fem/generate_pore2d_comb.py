62174492dfe4af45ecf4860cb92e00df6349e5f4
Fiorello Mino <fiorellomino@MacBook-Air-di-Fiorello.local>
2026-05-27 16:17:27 +0200

    fem

--- fem/generate_pore2d_comb.py
+++ fem/generate_pore2d_comb.py
@@ -285,7 +285,7 @@ def main():
     )
     parser.add_argument("-o", "--output", default="pore2D.dat")
     parser.add_argument("--n-inner-teeth", type=int, default=3)
-    parser.add_argument("--tooth-width", type=float, default=0.12)
+    parser.add_argument("--tooth-width", type=float, default=0.16)
     parser.add_argument("--tooth-height", type=float, default=0.22)
     parser.add_argument("--side-width", type=float, default=0.12)
     parser.add_argument("--side-height", type=float, default=0.50)
