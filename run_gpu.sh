273b4c2a01ef812058fee21a3dd35a15d1944982
Fiorello Mino <fiorellomino@192.168.1.54>
2026-05-29 21:25:38 +0200

    fem

---
+++ run_gpu.sh
@@ -0,0 +1,8 @@
+python train.py \
+  --debug \
+  --epochs 1 \
+  --batch 1 \
+  --device cuda \
+  --train_set /home/fiorello/master_thesis/machine_learning/train/training_set_64_from_5.txt \
+  --valid_set /home/fiorello/master_thesis/machine_learning/train/validation_set_64_from_5.txt \
+  --id sanity_cuda
