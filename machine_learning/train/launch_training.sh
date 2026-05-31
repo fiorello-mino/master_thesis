806423f7a33124c41735754154331e5f43dec0c6
fiorello-mino <fiorellomino01@gmail.com>
2026-05-31 23:01:29 +0200

    ext test

--- machine_learning/train/launch_training.sh
+++ machine_learning/train/launch_training.sh
@@ -13,7 +13,7 @@ python3 /home/fiorello/CRANE/train.py \
 	--weightd 1e-5 \
 	--train_set 'training_set_64_2_from_10.txt' \
 	--valid_set 'validation_set_64_2_from_10.txt' \
-	--id 'lr1e-4_b8_k3_hl2_ch16_seq_30' \
+	--id 'lr1e-4_b8_k3_hl2_ch16_seq_20_ramp5' \
 	--logfreq 1 \
 	--kernel_size 3 \
 	--hidden 2 \
@@ -21,12 +21,12 @@ python3 /home/fiorello/CRANE/train.py \
 	--nproc 4 \
 	--num_params 0 \
 	--subseq_min 5 \
-	--subseq_max 30 \
+	--subseq_max 20 \
 	--reflection \
 	--noise_reg 0 \
 	--rotation90 \
 	--divergence \
 	--dual \
-	--ramp_length 0
-	#--ramp 
+	--ramp_length 10 \
+	--ramp 
 	#--reload_model '/home/fiorello/master_thesis/machine_learning/train/train_logs/lr_5e-5_hl_3_2_from_10/model/epoch_279.pt'
