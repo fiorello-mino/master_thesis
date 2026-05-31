9e124f2d3bede6ac5d4996cf7c52988cc771deb0
fiorello-mino <fiorellomino01@gmail.com>
2026-05-31 02:38:14 +0200

    train

--- run_cpu.sh
+++
@@ -1,8 +0,0 @@
-python train.py \
-  --debug \
-  --epochs 1 \
-  --batch 1 \
-  --device cpu \
-  --train_set /home/fiorello/master_thesis/machine_learning/train/training_set_64_from_5.txt \
-  --valid_set /home/fiorello/master_thesis/machine_learning/train/validation_set_64_from_5.txt \
-  --id sanity_cpu
