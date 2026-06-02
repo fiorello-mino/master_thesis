5729e96557eb01b3014460f7a59ec1737aac193d
fiorello-mino <fiorellomino01@gmail.com>
2026-06-02 12:20:01 +0200

    ext test

--- machine_learning/train/launch_training.sh
+++ machine_learning/train/launch_training.sh
@@ -5,18 +5,18 @@ python3 /home/fiorello/CRANE/train.py \
 	--padding 'circular' \
 	--size 64 \
 	--seed 666 \
-	--epochs 200 \
+	--epochs 400 \
 	--nocrop \
 	--bias \
-	--lr 1e-4 \
+	--lr 5e-5 \
 	--batch 8 \
 	--weightd 1e-5 \
 	--train_set 'training_set_64_2_from_10.txt' \
 	--valid_set 'validation_set_64_2_from_10.txt' \
-	--id 'lr1e-4_b8_k5_hl3_ch16_seq20_ramp5_wd1e-5' \
+	--id 'lr5e-5_b8_k5_hl2_ch16_seq20_ramp5_wd1e-5' \
 	--logfreq 1 \
 	--kernel_size 5 \
-	--hidden 3 \
+	--hidden 2 \
 	--channels 16 \
 	--nproc 4 \
 	--num_params 0 \
