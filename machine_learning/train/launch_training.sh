74bb39ea616abec893752503b4e5168a65d9ee54
fiorello-mino <fiorellomino01@gmail.com>
2026-06-04 16:19:53 +0200

    ext test

--- machine_learning/train/launch_training.sh
+++ machine_learning/train/launch_training.sh
@@ -5,28 +5,28 @@ python3 /home/fiorello/CRANE/train.py \
 	--padding 'circular' \
 	--size 64 \
 	--seed 666 \
-	--epochs 300 \
+	--epochs 500 \
 	--nocrop \
 	--bias \
-	--lr 1e-4 \
-	--batch 4 \
+	--lr 5e-5 \
+	--batch 3 \
 	--weightd 2e-5 \
 	--train_set 'training_set_64_2_from_10.txt' \
 	--valid_set 'validation_set_64_2_from_10.txt' \
-	--id 'lr1e-4_b4_k5_hl2_ch16_seq20_ramp5_wd2e-5' \
+	--id 'lr5e-5_b3_k7_hl3_ch16_seq49_ramp48_wd2e-5' \
 	--logfreq 1 \
-	--kernel_size 5 \
-	--hidden 2 \
+	--kernel_size 7 \
+	--hidden 3 \
 	--channels 16 \
 	--nproc 4 \
 	--num_params 0 \
-	--subseq_min 5 \
-	--subseq_max 20 \
+	--subseq_min 1 \
+	--subseq_max 49 \
 	--reflection \
 	--noise_reg 0 \
 	--rotation90 \
 	--divergence \
 	--dual \
-	--ramp_length 5 \
+	--ramp_length 48 \
 	--ramp 
 	#--reload_model '/home/fiorello/master_thesis/machine_learning/train/train_logs/lr_5e-5_hl_3_2_from_10/model/epoch_279.pt'
