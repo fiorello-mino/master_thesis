1c45f6d98cbd44535487e654d6c1ced44163b2ad
Fiorello Mino <fiorellomino@192.168.1.54>
2026-06-09 21:34:51 +0200

    fem

--- fem/generate_pore2d_comb.py
+++ fem/generate_pore2d_comb.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import numpy as np
+import random
+
+
 
 def load_pore2d(path: Path) -> str:
     
@@ -8,6 +11,7 @@ def load_pore2d(path: Path) -> str:
     
     text = path.read_text()
     return text
+
 
 
 def get_phi_block(path: Path):
@@ -29,6 +33,7 @@ def get_phi_block(path: Path):
     after = text[end_idx:]
     
     return before, phi_block, after
+    
     
 
 def build_shape_line(n_teeth: int) -> str:
@@ -57,6 +62,7 @@ def generate_base_rectangle(base_sides, base_center):
     return block
 
 
+
 def generate_tooth_rectangle(i: int, rectangle_sides, x_center, y_center):
     
     lines = []
@@ -69,29 +75,54 @@ def generate_tooth_rectangle(i: int, rectangle_sides, x_center, y_center):
     return block
 
 
-def generate_all_teeth(n_teeth: int, rectangle_sides, x_min, x_max, y_center):
-    
+
+def generate_all_teeth(
+    n_teeth: int,
+    epsilon: float,
+    x_min: float,
+    x_max: float,
+    y_center: float,
+    height_min: float,
+    height_max: float,   
+    width_max: float,
+    gap_min: float                    
+):
     blocks = []
+
+    L = x_max - x_min
+
+    while True:
+        widths = [random.uniform(2*epsilon, width_max) for _ in range(n_teeth)]
+        total_width = sum(widths) + (n_teeth - 1) * gap_min
+        if total_width <= L:
+            break
     
-    # rettangoli pettine
-    centers_distance = (x_max - x_min) / n_teeth
-    x_centers = np.arange(x_min, x_max, centers_distance) + 0.5*centers_distance
+    cur_x = x_min + gap_min
     
-    for i in range(1, n_teeth+1):
-        blocks.append(generate_tooth_rectangle(i, rectangle_sides, x_centers[i-1], y_center))
+    for i in range(n_teeth):
         
+        if i > 0:
+            cur_x += gap_min
+            
+        Lx = widths[i]
+        Ly = random.uniform(height_min, height_max)
+
+        rectangle_sides = (Lx, Ly)
+        x_center = cur_x + 0.5*Lx
+        block_i = generate_tooth_rectangle(i+1, rectangle_sides, x_center, y_center)
+        blocks.append(block_i)
+        
+        cur_x += Lx
+
     return "\n".join(blocks)
 
     
+    
 def generate_phi_block(
-    n_teeth: int, 
-    base_sides, 
-    base_center, 
-    rectangle_sides, 
-    x_min, 
-    x_max, 
-    y_center
+    epsilon: float,
+    width_max: float
 ) -> str:
+    
     
     lines = []
     
@@ -101,16 +132,36 @@ def generate_phi_block(
     lines.append("surf->phi->external file:				init/test.arh")
     lines.append("surf->phi->constant:                            1.")
     lines.append(" ")
+    
+    x_max = 0.5
+    x_min = -0.5
+    L_base_x = 2.0
+    L_base_y = random.uniform(0.2, 0.8)
+    base_sides = (L_base_x, L_base_y)
+    center_base_x = 0.0
+    center_base_y = 0.5
+    base_center = (center_base_x, center_base_y)
+    
+    n_teeth = random.randint(2, 6)
+    height_min = L_base_y + 0.1
+    height_max = 2.0 - L_base_y
+    y_center = 0.5
+    gap_min = 2*epsilon
+    
     lines.append(build_shape_line(n_teeth))
-    lines.append("surf->phi->shape->inner value:                  1")
-    lines.append("surf->phi->shape->outer value:                  0")
+    lines.append("surf->phi->shape->inner value:                  0")
+    lines.append("surf->phi->shape->outer value:                  1")
     lines.append("surf->phi->shape->center:		        [ 0. , 0. ]")
     lines.append(" ")
     lines.append("surf->phi->shape->eps:                          ${surf->eps}")
     lines.append(" ")
     
     base_block = generate_base_rectangle(base_sides, base_center)
-    teeth_block = generate_all_teeth(n_teeth, rectangle_sides, x_min, x_max, y_center)
+    teeth_block = generate_all_teeth(
+        n_teeth, epsilon, x_min, x_max, y_center,
+        height_min, height_max, width_max, gap_min
+    )
+    
     lines.append(base_block)
     lines.append(teeth_block)
     lines.append(" ")
@@ -118,8 +169,7 @@ def generate_phi_block(
     block = "\n".join(lines)
     return block
     
-    
-
+        
         
 def main():
     
@@ -127,13 +177,8 @@ def main():
     before, phi_block, after = get_phi_block(path)
     
     new_phi_block = generate_phi_block(
-        n_teeth = 4,
-        base_sides = (1.2, 0.4),
-        base_center = (0, 0.5),
-        rectangle_sides = (0.1, 0.4),
-        x_min = -0.5,
-        x_max = 0.5,
-        y_center = 0.4
+        epsilon = 0.0115,
+        width_max = 0.3
     )
     
     new_text = before + new_phi_block + after
