9bcec7c1814f25f3112dc95494d3a5f9194e1293
Fiorello <fiorellomino01@gmail.com>
2026-07-01 13:52:54 +0200

    fem

---
+++ machine_learning/train_pores/launch_training.sh
@@ -0,0 +1,37 @@
+# !/bin/bash
+
+python3 /home/fiorello/CRANE_bc/train.py \
+	--device  'cuda:0' \
+	--padding 'circular,reflect' \
+	--size 64 \
+	--seed 666 \
+	--epochs 500 \
+	--nocrop \
+	--bias \
+	--lr 5e-5 \
+	--batch 3 \
+	--weightd 0e-5 \
+	--train_set 'train_set.txt' \
+	--valid_set 'valid_set.txt' \
+	--id 'bc_y' \
+	--logfreq 1 \
+	--kernel_size 5 \
+	--hidden 3 \
+	--channels 16 \
+	--nproc 4 \
+	--num_params 0 \
+	--subseq_min 1 \
+	--subseq_max 49 \
+	--reflection \
+	--noise_reg 0 \
+	--rotation90 \
+	--divergence \
+	--dual \
+	--massW 2.0 \
+	--coeffE 1e-1 \
+	--coeffG 1e-5 \
+        --eps 0.078125 \
+	--dx 0.015625 \
+	--ramp \
+	--ramp_length 48 \
+	--reload_model '/home/fiorello/master_thesis/machine_learning/train_mod/train_logs/coeffE1e-1/model/epoch_476.pt'
