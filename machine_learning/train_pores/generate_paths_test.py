aa5d3e77ac17292e67ce300583bc69d518aa154e
Fiorello Mino <fiorellomino@192.168.1.54>
2026-07-03 01:26:26 +0200

    fem

--- machine_learning/train_pores/generate_paths_test.py
+++ machine_learning/train_pores/generate_paths_test.py
@@ -8,7 +8,7 @@ n_folders = 100
 with open(output_txt, "w") as f:
     for folder_idx in range(n_folders):
         folder = base_dir / f"{folder_idx:03d}"
-        start_index = 0
+        start_idx = 0
         # indice → tempo in step da 0.1
         times = [start_idx * 0.1 + k * 0.1 for k in range(201)]
         # formattazione 1 decimale: 0.0, 0.1, ..., 20.0
