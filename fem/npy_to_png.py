db5e09d27de3be44e7357fc9220441d69afcd176
Fiorello Mino <fiorellomino@192.168.1.54>
2026-08-26 04:29:36 +0200

    test

---
+++ fem/npy_to_png.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Carica il file allungato
+surf = np.load('/scratch/fiorello/data_test/pores_replicated_npy/000/surf_0.0.npy')
+print("Forma:", surf.shape)
+
+# Normalizza tra 0 e 1 per il salvataggio come immagine
+#surf_norm = (surf - surf.min()) / (surf.max() - surf.min())
+
+# Salva come PNG (scala di grigi)
+plt.imsave('surf_000.png', surf_norm, cmap='gray')
+print("Salvato: surf_000.png")
