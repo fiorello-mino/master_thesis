132aa147905b06a93d270f3012b58ffb116543c2
Fiorello <fiorellomino01@gmail.com>
2026-09-05 23:04:14 +0200

    .

--- fem/3D/tools/find_shortest_simulation.py
+++ fem/3D/tools/find_shortest_simulation.py
@@ -19,7 +19,7 @@ VTK_MASK_SUFFIX = "_vtk_fallback_mask.npy"
 
 # Numero minimo di frame che una simulazione deve contenere per essere mantenuta.
 # Le cartelle con n_frames < N_FRAMES sono considerate invalide.
-N_FRAMES = 100
+N_FRAMES = 20
 
 # SICUREZZA:
 # False -> stampa e crea solo il report, NON elimina nulla.
