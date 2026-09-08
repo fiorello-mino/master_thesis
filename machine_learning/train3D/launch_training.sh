e9e1007d4bb7f03a01ac7e322b4769d19254b3af
Fiorello <fiorellomino01@gmail.com>
2026-09-08 18:13:11 +0200

    .

--- machine_learning/train3D/launch_training.sh
+++ machine_learning/train3D/launch_training.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-python3 /home/fiorello/CRANE/train.py \
+python3 /home/fiorello/CRANE/train_with_loss.py \
 	--device  'cuda:0' \
 	--threeD \
 	--vtk \
@@ -14,7 +14,7 @@ python3 /home/fiorello/CRANE/train.py \
 	--weightd 0e-5 \
 	--train_set 'train_set.txt' \
 	--valid_set 'valid_set.txt' \
-	--id 'prova3D' \
+	--id 'provaLoss' \
 	--logfreq 1 \
 	--kernel_size 5 \
 	--hidden 3 \
